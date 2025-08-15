923e616c7f5dd2070fc97fb2c703b4819d093338
sambhav291 <sambhavmagotra009@gmail.com>
2025-08-16 05:09:56 +0530

    Bugs 94

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,10 +9,11 @@ import './index.css';
 import './App.css';
 import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url,
+// ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 
 // Get the root element from the DOM
 const rootElement = document.getElementById('root');
@@ -36,63 +37,4 @@ root.render(
 
 
 
-
-
-
-
-
-// import React, { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import App from './App';
-// import { AuthProvider } from './context/AuthContext';
-// import { ScrollProvider } from "./context/ScrollContext";
-// import './index.css';
-// import './App.css';
-// import "./pdf-worker";
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <AuthProvider>
-//       <ScrollProvider>
-//         <BrowserRouter>
-//           <App />
-//         </BrowserRouter>
-//       </ScrollProvider>
-//     </AuthProvider>
-//   </StrictMode>
-// );
-
-
-
-
-
-
-
-
-// import React, { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
-// import Dashboard from './components/Dashboard';
-// import { AuthProvider } from './context/AuthContext';
-// import { ScrollProvider } from "./context/ScrollContext";
-// import Navbar from './components/Navbar';
-// import './index.css';
-// import './App.css';
-// import "./pdf-worker";
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <AuthProvider>
-//       <ScrollProvider>
-//         <BrowserRouter>
-//           <Navbar/>
-//           <Dashboard />
-//         </BrowserRouter>
-//       </ScrollProvider>
-//     </AuthProvider>
-//   </StrictMode>
-// );
 
